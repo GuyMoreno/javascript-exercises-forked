42d28b8d28bec00d53dbffb56892f95d5ac79339
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-04 12:44:35 +0200

    finished reverse func

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    // creating an array that every element is a letter.
+    let splitString = string.split("");
+    // revese the order of the letters.
+    let reversedAraay = splitString.reverse();
+    // join
+    let joinArray = reversedAraay.join("");
 
+    return joinArray;
 };
+console.log(reverseString("Hello World")); // dlroW olleH
 
 // Do not edit below this line
 module.exports = reverseString;
