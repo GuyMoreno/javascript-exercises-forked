c552eb6f0584df5f79611ac48494792f47e4e021
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-05 13:41:09 +0200

    passed 5/7 tests need to handle non-intger and string inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    // innizialize result
+    let result = 0;
+
+    // check if order of parmaters wrong
+    if (num1 > num2) { //we don't want 👈🏽
+        // so change the paras to array
+        // and then switch beween if needed
+        [num1, num2] = [num2, num1];
+    }
+    if (num1 >= 0 && num2 >= 0) {
+        for (let i = num1; i <= num2; i++) {
+            result += i;
+
+        }
+    } else {
+        return "ERROR";
+    }
+    return result;
 
 };
 
