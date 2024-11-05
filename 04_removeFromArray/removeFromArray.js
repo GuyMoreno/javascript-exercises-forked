f95d02e00a9484acff6f8e32c41e1baf9a20f418
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-05 11:48:53 +0200

    finished removeFromArray func

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...numsToRemove) {
+    const copiedArray = array.slice();
+
+    for (let num of numsToRemove) {
+        let index = copiedArray.indexOf(num);
+        while (index !== -1) {
+            copiedArray.splice(index, 1);
+            index = copiedArray.indexOf(num);
+        }
+
+    }
+    return copiedArray;
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
