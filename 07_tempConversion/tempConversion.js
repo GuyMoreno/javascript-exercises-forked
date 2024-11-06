7c6f663cc191c38fdce5602144d938f125a65b48
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-06 14:26:19 +0200

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenhit) {
+  let result = 0;
+  result = (fahrenhit - 32) * 5 / 9;
+  return parseFloat(result.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = 0;
+  result = celsius * 9 / 5 + 32;
+  return parseFloat(result.toFixed(1));
 };
+
 
 // Do not edit below this line
 module.exports = {
