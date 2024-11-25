16c80bab75473a140c121edddda6d941f400ed5c
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-25 11:56:19 +0200

    finish all function for due check

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+// for multiple numbers
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0)
+}
+
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const power = function (a, b) {
+  return Math.pow(a, b);
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (n) {
+  let product = 1;
+  for (let i = n; i < 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
