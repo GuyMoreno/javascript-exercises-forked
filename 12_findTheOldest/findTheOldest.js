c45c63c6e8e889b339d9bd92771d6d7bafcb9d4f
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-26 13:58:54 +0200

    finished findOledest but testing due

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
 
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
+
+const findTheOldest = function (people) {
+    // check object object 1 by 1
+    return people.reduce((oldest, currentPerson) => {
+        // pass the first func from above
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
